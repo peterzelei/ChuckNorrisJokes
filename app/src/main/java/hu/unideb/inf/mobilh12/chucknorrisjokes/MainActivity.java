1d42b0935466e3fa0f2430586a5e9a1d6ab52a8c
peterzelei <zeleipeter21@gmail.com>
2022-12-07 18:33:27 +0100

    Working API GET request with Volley

--- app/src/main/java/hu/unideb/inf/mobilh12/chucknorrisjokes/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/mobilh12/chucknorrisjokes/MainActivity.java
@@ -3,11 +3,49 @@ package hu.unideb.inf.mobilh12.chucknorrisjokes;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import hu.unideb.inf.mobilh12.chucknorrisjokes.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    public ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        binding.getJokeButton.setOnClickListener(button -> getNewJoke());
+    }
+
+    private void getNewJoke() {
+        RequestQueue queue = VolleySingleton.getInstance(this).getRequestQueue();
+        String url = "https://api.chucknorris.io/jokes/random";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                binding.jokeTextView.setText("Response: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Handle error
+            }
+        }
+        );
+
+        queue.add(jsonObjectRequest);
     }
 }
